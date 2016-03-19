57c0bdffa1980531fe2185ee4df8d90e1fd1f563
i1kondratiuk <i1kondratiuk@gmail.com>
2016-03-19 15:47:15 +0200

    added parameters to the methods

--- Input.java
+++ Input.java
@@ -1,5 +1,7 @@
 package homework;
 
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Input {
@@ -13,16 +15,25 @@ public class Input {
 
 	/**
 	 * Display information about a member of the club to the console
+	 * 
+	 * @param name
 	 */
-	public void infoAboutClubMember() {
-
+	public void infoAboutClubMember(ClubMember name) {
+		System.out.println("The name of the member of the club - " + name);
 	}
 
 	/**
 	 * Display information about a member's pets to the console
+	 * 
+	 * @param name
+	 * @param zooclub
 	 */
-	public void infoAboutClubMembersPets() {
-
+	public void infoAboutClubMembersPets(ClubMember name, Map<ClubMember, ArrayList<Pet>> zooclub) {
+		for (Map.Entry<ClubMember, ArrayList<Pet>> zoo : zooclub.entrySet()) {
+			if (zoo.getKey().equals(name)) {
+				System.out.println(zoo.getValue());
+			}
+		}
 	}
 
 	/**
